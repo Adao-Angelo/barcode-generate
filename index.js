363ebd185ba7d78dbb187d182d5327b827648ed6
Adao-Angelo <adaobegginer@gmail.com>
2024-03-02 16:05:26 +0100

    config script dev

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require("express");
-const {Canvas } = require("canvas");
+const { Canvas } = require("canvas");
 const jsBarcode = require("jsbarcode");
 
+const app = express();
 
-const app = express()
-
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:false}));
-app.use(express.json())
-app.get("/" , (request ,resposne)=>{
-    resposne.render("index", {data:""})
-})
-app.post("/" ,(req , res)=>{
-    const canvas = new Canvas()
-    jsBarcode(canvas , req.body.qrcodee , {
-        lineColor:"#000",
-        width:4,
-        height:40,
-        displayValue:false
-    });
-   canvas.toDataURL("image/png" , (err , png)=>{
-    res.render("index", {data:png});
-   })
-})
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.get("/", (request, resposne) => {
+  resposne.render("index", { data: "" });
+});
+app.post("/", (req, res) => {
+  const canvas = new Canvas();
+  jsBarcode(canvas, req.body.qrcodee, {
+    lineColor: "#000",
+    width: 4,
+    height: 40,
+    displayValue: false,
+  });
+  canvas.toDataURL("image/png", (err, png) => {
+    res.render("index", { data: png });
+  });
+});
 const port = process.env.PORT || 2000;
-app.listen(port, console.log('server run at port', port));
+app.listen(port, console.log("server run at port", port));
